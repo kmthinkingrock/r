79851ba6f88c3d0ccab8290251d6bab7cd4cd611
Kay McCormick <kay.elearning@gmail.com>
2015-12-04 00:25:05 -0800

    added xlsx output

--- python/q.py
+++ python/q.py
@@ -3,6 +3,12 @@ import codecs
 import json
 import csv
 import cStringIO
+import xlsxwriter
+
+#colPath = "/sdcard/collection.anki2"
+colPath = "/home/kay/collection.anki2"
+#outdir = '/sdcard/work/'
+outPath = "./out/"
 
 class UnicodeWriter:
   """ A CSV writer which will write rows to CSV file "f", which is encoded in the given encoding. """
@@ -29,40 +35,59 @@ class UnicodeWriter:
     for row in rows:
       self.writerow(row)
       
-outdir = '/sdcard/work/'
-fmodel = codecs.open(outdir + 'models.txt', 'w', 'utf-8')
-conn = sqlite3.connect("/sdcard/collection.anki2")
+fmodel = codecs.open(outPath + 'models.txt', 'w', 'utf-8')
+conn = sqlite3.connect(colPath)
 c=conn.cursor()
 c.execute("select models from col")
 rw=c.fetchone()
 x=json.loads(rw[0])
 print x.keys()
+workbook = xlsxwriter.Workbook(outPath + 'cards.xlsx')
 for mid,v in x.iteritems():
   print mid
   fmodel.write(str(v) + "\n")
   r=c.execute("select flds from notes where mid = ?", (mid, ))
+  worksheet = workbook.add_worksheet()
   flds=v['flds']
   
   #w = csv.writer(f2, delimiter = ',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-  outfname = "/sdcard/work/" + v['name'] + ".txt"
-  f2 = open("/sdcard/work/" + v['name'] + ".csv", 'w')
+  outfname = outPath + v['name'] + ".txt"
+  f2 = open(outPath + v['name'] + ".csv", 'w')
   w = UnicodeWriter(f2)
   f = codecs.open(outfname, 'w', 'utf-8')
   firstf = True
+  row = 0
+  col = 0
   for fld in flds:
     if firstf == False:
       f.write("\t")
     firstf = False
     f.write(fld['name'])
+    worksheet.write(row, col, fld['name'])
+    col += 1
   f.write("\n")
-  for row in r:
-    rf=row[0].split("\x1f")
+  row += 1
+  col = 0
+  for orow in r:
+    rf=orow[0].split("\x1f")
+    for rff in rf:
+#      print rff
+#      if isinstance(rff,str):
+#        print "string"
+      x12 = codecs.encode(rff, 'utf-8')
+      worksheet.write(row, col, rff)
+      col += 1
+
+    row += 1
+    col = 0
     w.writerow(rf)
-    p=row[0].replace("\x1f","\t")
+    p=orow[0].replace("\x1f","\t")
     f.write(p)
     f.write("\n")
   f.close()
   
   f2.close()
 fmodel.close()
+workbook.close()
+
 
